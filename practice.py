2f3005ca639e32d487ec16f578b4eab7fbbfb0f8
DKcodeX <148317495+DkCodeProjct@users.noreply.github.com>
2024-01-21 15:40:54 +0530

    this is just a psudoCode
    cs50 problem very happy i wrote this my self

--- practice.py
+++ practice.py
@@ -13,6 +13,7 @@ with open(inputFile, 'r') as file:
     output_file.close()
     
     for line in file:
+        
         name1, name2, house = line.split(', ')
         student.append({'name1':name1, 'name2':name2, 'house':house})
         output_file = open(sys.argv[2], 'w')
