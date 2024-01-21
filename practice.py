f31c132b54f4346d5269ac2f3f43ef2c9c84df52
DKcodeX <148317495+DkCodeProjct@users.noreply.github.com>
2024-01-21 15:38:13 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -4,11 +4,14 @@ import csv
 inputFile = sys.argv[1]
 outputFile = sys.argv[2]
 student = []
+
 with open(inputFile, 'r') as file:
+    
     output_file = open(sys.argv[2], 'w')
     writer = csv.writer(output_file)
     writer.writerow(['first', 'last', 'house'])
     output_file.close()
+    
     for line in file:
         name1, name2, house = line.split(', ')
         student.append({'name1':name1, 'name2':name2, 'house':house})
