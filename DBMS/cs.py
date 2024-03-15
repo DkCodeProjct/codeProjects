620d92da9d5da45d7473dbcba0cb54304915bc8b
DKcodeX <148317495+DkCodeProjct@users.noreply.github.com>
2024-03-15 13:36:39 +0530

    Update cs.py
    add code to give user a ID,,,, but GPT wrote it,
    now im depressed,,
    but i can get over with it and, make another project and understand it properly

--- DBMS/cs.py
+++ DBMS/cs.py
@@ -2,15 +2,33 @@ import csv
 from tabulate import tabulate
 
 class PsycholDataBase:
-    def __init__(self):
-        
+     def __init__(self):
+        self.currentID = 0
         self.data = []
         self.filename = "pychoDB.csv"
+        self.addId()
+
+    def addId(self):
+        with open(self.filename, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                self.data.append(row)
+                self.currentID = max(self.currentID, int(row['Id']))
+    
+    
+    def save_data(self, new_data):
+        with open(self.filename, 'a') as file:
+            writer = csv.DictWriter(file, fieldnames=['Id', 'Name', 'Age', 'civilStatus', 'Deseas', 'TherapySesions'])
+            if file.tell() == 0:
+                writer.writeheader()
+            writer.writerow(new_data)
+
         
     def getData(self): 
 # wtf i put single qute in feilds and put "" in enter data {name..} ERROR occured
-        feilds = ['Name','Age',"civilStatus", "Deseas",'TherapySesions']
+        feilds = ['Id','Name','Age',"civilStatus", "Deseas",'TherapySesions']
         enterData = {
+            'Id':self.currentID +1,
             'Name':input('Name '),
             'Age':input("Age "),
             "civilStatus":input('civil status '),
@@ -19,8 +37,8 @@ class PsycholDataBase:
 
         }
         
+        self.currentID += 1
         self.data.extend(dict(enterData))
-
         with open(self.filename, 'a') as file:
             writer = csv.DictWriter(file,fieldnames=feilds)
             if file.tell() == 0:
